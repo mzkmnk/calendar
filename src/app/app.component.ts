e80f4ea21453907ac76141fb370b3a006c09e4cf
mzkmnk <sma0729mex@gmail.com>
2024-04-28 18:05:44 +0900

    update:フォルダ構成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonContent,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonLabel, IonContent, IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   constructor() {}
